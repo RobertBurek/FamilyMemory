de0e3f7dcfd89278a1a8ca73dec9bbb08effb403
Robert Burek <robertburek@wp.pl>
2021-09-26 14:25:39 +0200

    cardsCatAll oraz losowe rozmieszczenie kart kotów

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const cardsDogAll = ["puppy-1.jpg", "puppy-2.jpg", "puppy-3.jpg", "puppy-4.jpg", "puppy-5.jpg", "puppy-6.jpg", "puppy-7.jpg", "puppy-8.jpg", "puppy-9.jpg",
-                     "puppy-10.jpg", "puppy-11.jpg", "puppy-12.jpg"];
+const cardsDogAll = ["dogs/puppy-1.jpg", "dogs/puppy-2.jpg", "dogs/puppy-3.jpg", "dogs/puppy-4.jpg", "dogs/puppy-5.jpg", "dogs/puppy-6.jpg", "dogs/puppy-7.jpg", "dogs/puppy-8.jpg", 
+                    "dogs/puppy-9.jpg", "dogs/puppy-10.jpg", "dogs/puppy-11.jpg", "dogs/puppy-12.jpg"];
+const cardsCatAll = ["cats/kitty-1.jpg", "cats/kitty-2.jpg", "cats/kitty-3.jpg", "cats/kitty-4.jpg", "cats/kitty-5.jpg", "cats/kitty-6.jpg", "cats/kitty-7.jpg", "cats/kitty-8.jpg",
+                    "cats/kitty-9.jpg", "cats/kitty-10.jpg", "cats/kitty-11.jpg", "cats/kitty-12.jpg", "cats/kitty-13.jpg", "cats/kitty-14.jpg"];
 let cardsGame =[];
 
 function randomCards(elments){
@@ -16,6 +18,9 @@ function randomCards(elments){
 
 randomCards(cardsDogAll);
 console.log(cardsGame);
+cardsGame =[];
+randomCards(cardsCatAll);
+console.log(cardsGame);
 
 let cards = document.querySelectorAll("div");
 cards = [...cards];
@@ -23,7 +28,7 @@ cards = [...cards];
 // const init = function() {
     cards.forEach(function(card){
         const position = Math.floor(Math.random() * cardsGame.length);
-        card.style.backgroundImage = "url(img/dogs/"+cardsGame[position];
+        card.style.backgroundImage = "url(img/"+cardsGame[position];
         card.style.backgroundSize = "100% 100%";
         cardsGame.splice(position,1);
     }
