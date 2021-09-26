c2cc63e81c33bf2139029a841306708add519352
Robert Burek <robertburek@wp.pl>
2021-09-26 14:17:06 +0200

    Obrazki kotków

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const cardsDogAll = ["puppy-1.jpg", "puppy-2.jpg", "puppy-3.jpg", "puppy-4.jpg",
 let cardsGame =[];
 
 function randomCards(elments){
-    console.log(elments.length);
+    // console.log(elments.length);
     for (var step = 0; step < 9; step++) {
         var index = Math.floor(Math.random() * elments.length);
         cardsGame.push(elments[index]);
         cardsGame.push(elments[index]);
-        console.log(elments[index]);
+        // console.log(elments[index]);
         elments.splice(index, 1);
     }
     // console.log(elments);
@@ -16,3 +16,16 @@ function randomCards(elments){
 
 randomCards(cardsDogAll);
 console.log(cardsGame);
+
+let cards = document.querySelectorAll("div");
+cards = [...cards];
+
+// const init = function() {
+    cards.forEach(function(card){
+        const position = Math.floor(Math.random() * cardsGame.length);
+        card.style.backgroundImage = "url(img/dogs/"+cardsGame[position];
+        card.style.backgroundSize = "100% 100%";
+        cardsGame.splice(position,1);
+    }
+    );
+// }
