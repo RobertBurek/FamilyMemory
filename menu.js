23e17c3cdd2d21499137c35a5d1bc76645b2a9c0
Robert Burek <robertburek@wp.pl>
2021-10-21 22:29:20 +0200

    Menu cd..

---
+++ menu.js
@@ -0,0 +1,7 @@
+let cat = document.getElementById("cat");
+let dog = document.getElementById("dog");
+let but = document.getElementById("butterfly");
+let el12 = document.getElementById("el12");
+let el18 = document.getElementById("el18");
+let el24 = document.getElementById("el24");
+
