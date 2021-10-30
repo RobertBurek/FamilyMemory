cc13010c6afd4705885abc63253e1b7c7422a687
Robert Burek <robertburek@wp.pl>
2021-10-30 20:46:36 +0200

    Prezentacja wyników cd...

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 const animalClasses = ["kitty11", "puppy8", "butterfly10"];
 const levelClasses = ["elements12", "elements18", "elements24"];
-const top3Classes = ["top3_1", "top3_2", "top3_3"];
+const topResultClasses = ["topResult_1", "topResult_2", "topResult_3"];
 
 let animal1 = document.getElementById("animal1");
 let animal2 = document.getElementById("animal2");
@@ -8,17 +8,18 @@ let animal3 = document.getElementById("animal3");
 let level1 = document.getElementById("level1");
 let level2 = document.getElementById("level2");
 let level3 = document.getElementById("level3");
-let top3_1 = document.getElementById("top3_1");
-let top3_2 = document.getElementById("top3_2");
-let top3_3 = document.getElementById("top3_3");
+let topResult_1 = document.getElementById("topResult_1");
+let topResult_2 = document.getElementById("topResult_2");
+let topResult_3 = document.getElementById("topResult_3");
 let divCenter = document.getElementById("center");
-let pictures = document.getElementById("pictures");
-let top3 = document.getElementById("top3");
+let divPictures = document.getElementById("pictures");
+let divLevels = document.getElementById("levels");
+let divTopResult = document.getElementById("topResult");
 
 
 let listPictures = [animal1, animal2, animal3];
 let listLevels = [level1, level2, level3];
-let listTop3 = [top3_1, top3_2, top3_3]
+let listTopResult = [topResult_1, topResult_2, topResult_3];
  
 for (let i = 0; i < listPictures.length; i++) {
     listPictures[i].classList.add(animalClasses[i]);
@@ -29,9 +30,9 @@ for (let i = 0; i < listLevels.length; i++) {
     listLevels[i].classList.add("display");
     listLevels[i].classList.add("viewHover");
 }
-for (let i = 0; i < listTop3.length; i++) {
-    listTop3[i].classList.add(top3Classes[i]);
-    listTop3[i].classList.add("display");
+for (let i = 0; i < listTopResult.length; i++) {
+    listTopResult[i].classList.add(topResultClasses[i]);
+    listTopResult[i].classList.add("display");
 }
 
 localStorage.clear();
@@ -42,6 +43,7 @@ function chosenPlayer(chosen){
     listLevels.forEach(level => {
         level.classList.toggle('display');
     })
+    divLevels.classList.add('levels');
     listPictures.forEach(picture => {
         picture.classList.toggle('viewHover');
     })
@@ -52,9 +54,10 @@ function chosenPlayer(chosen){
     for (let i = 0; i < listPictures.length; i++) {
         listPictures[i].classList.remove(animalClasses[i]);
     }
-    listTop3.forEach((top3)=>{
-        top3.classList.remove("display");
+    listTopResult.forEach((topResult)=>{
+        topResult.classList.remove("display");
     })
+    divTopResult.classList.add('topResult');
     animal2.classList.add(classChosen);
     animal2.classList.add("blackFrame");
     animal1.removeEventListener("click", clickAnimal1);
@@ -68,16 +71,16 @@ function clearChosenPlayer(chosen){
     const classChosen = chosen.className;
     animal2.classList.remove(classChosen);
     divCenter.classList.remove('centerLevel');
-    pictures.classList.toggle('width50');
-    pictures.classList.toggle('width100');
+    divPictures.classList.toggle('width50');
+    divPictures.classList.toggle('width100');
     for (let i = 0; i < listPictures.length; i++) {
         listPictures[i].classList.add(animalClasses[i]);
     }
     listLevels.forEach(level => {
         level.classList.toggle('display');
     })
-    listTop3.forEach((top3)=>{
-        top3.classList.add("display");
+    listTopResult.forEach((topResult)=>{
+        topResult.classList.add("display");
     })
     listPictures.forEach(picture => {
         picture.classList.toggle('viewHover');
