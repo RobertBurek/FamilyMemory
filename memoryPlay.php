a3ac52343577a3bb9651bc22ffea82a7a25a251a
RobertB <robertburek@wp.pl>
2021-12-03 22:06:36 +0100

    Refaktor saveResults.php

--- memoryPlay.php
+++ memoryPlay.php
@@ -28,8 +28,8 @@
             <form action="saveResults.php" method="POST">
                 <p>
                     Podaj swoje imię: 
-                    <input id=name type="text" class="boxstyle" name="name" value="Robert" autofocus="autofocus">
-                    <input id="result" type="text" name="result" value="10" hidden/>
+                    <input id=name type="text" class="boxstyle" name="name" value="Martusia" autofocus="autofocus">
+                    <input id="result" type="text" name="result" value="9.125" hidden/>
                     <input id="level" type="text" name="level" value="cardsCatAll12" hidden/>
                 </p>
                 <a class="close" href="#">&times;</a>
--- saveResults.php
+++ saveResults.php
@@ -1,13 +1,11 @@
 <?php
-
     $result = sprintf("%01.3f", $_POST["result"]);
     $level = $_POST["level"];
     $name = stripslashes(trim($_POST["name"]));
 
-        // header("Location: http://localhost/FamilyMemory/index.php");
-        header("Location: http://localhost/MemoryPHP/memoryPlay.php");
-        // $level = $_POST["lista"];
-
+    // header("Location: http://localhost/MemoryPHP/index.php");
+    header("Location: http://localhost/MemoryPHP/memoryPlay.php");
+    // $level = $_POST["lista"];
 
     $name_file = 'results.txt';
     $default_file = 'defaultResults.php';
@@ -23,95 +21,42 @@
         {
             $line = str_replace("\n", "", fgets($fp));
             $line = str_replace("\r", "", $line);
-        //    $line = substr(fgets($fp), 0, -1);
-        //    $line = str_replace('\n', '', str_replace('\r', '', fgets($fp)));
            if ($line == $level) {
                $thisLevel = true;
-            //    $nextResult = 0;
            }
            if ($thisLevel) {
                if (($nextResult > 2) && ($nextResult % 2 == 1) && ($nextResult <= 22)) {
                     if ($result <= $line && !$changePlayer) {
-                        // array_push($results, $result.'\r\n');  //  .'\n'
-                        // array_push($results, $name.'\r\n');  //  .'\n'
                         array_push($results, $result);
                         array_push($results, $name);
-                        // $afterChange = true;
                         $changePlayer = true;
                     }
                }
-            //    if (($nextResult > 4) && ($nextResult % 2 == 0) && ($nextResult < 23)) {
-            //         if ($changePlayer && !$afterChange) {
-            //         array_push($results, $name);
-            //         $changePlayer = false;
-            //         !$afterChange = true;
-            //         };
-            //     }
-            //    ++$nextResult;
                array_push($results, $line);
                if ($changePlayer && $nextResult == 22) {
                    array_pop($results);
                    array_pop($results);
                };
-            //    if ($nextResult < 21) array_push($results, $line);
                if ($nextResult == 22) $thisLevel = false;
                ++$nextResult;
-            // } else array_push($results, $line);
             } else array_push($results, $line);
-        //    if ($thisLevel && ($nextResult == 22 || $nextResult == 23)) {
-            // if (!$thisLevel)// && ($nextResult == 22 || $nextResult == 23)) {
-            //  array_push($results, $line);
-        //    } else array_push($results, $line);
         };
         fclose($fp);
         unset($results[count($results)-1]);
     }
     else{
-    //   echo 'Nie znaleziono pliku!';
-    //   require_once $default_file;
+      echo 'Nie znaleziono pliku!';
+      require_once $default_file;
     }
-
-
-
-// $result = $_POST["result"];
-// $level = $_POST["level"];
-// $name = stripslashes(trim($_POST["name"]));
-// echo json_encode($results);
-
-// if ($result<>"" && $level<>"") {
-//   $tresc="$result - $level\n";
-//   $tresc=$tresc.$name."\n";
-$tresc = "";
-// $f=fopen("results.txt", "a");
-// ftruncate($f, 0);
-if ($results<>"") {
-    foreach ($results as $result) {
-        // if ($result === 'Kasia') $tresc = $tresc."Katarzyna\n";
-        // else 
-        // $tresc = $tresc.$result.'\r\n';//."\n";
-        $tresc = $tresc.$result."\n";//."\n";
-        // if ($result === 'cardsCatAll18') {
-            // $.'cardsCatAll18'='to jest to';
-            // echo $.'cardsCatAll18';
-            // define('STALA', array('ergerg'));
-            // echo STALA[0];
-        // }
-        // fputs($f, $result.'\n');
+    $tresc = "";
+    if ($results<>"") {
+        foreach ($results as $result) {
+            $tresc = $tresc.$result."\n";
+        }
+        $f=fopen("results.txt", "a");
+        ftruncate($f, 0);
+        fputs($f, $tresc);
+        fclose($f);
     }
-//   $tresc="$result - $level\n";
-//   $tresc=$tresc.$name."\n";
-    $f=fopen("results.txt", "a");
-    ftruncate($f, 0);
-    fputs($f, $tresc);
-    fclose($f);
-
-    // header("Location: http://localhost/FamilyMemory/index.php");
 //   mail("robertburek@wp.pl", "Wygrał dzisiaj", $tresc);
-}
-// fclose($f);
-//   $f=fopen("testResults.txt", "a");
-//   fputs($f, $results);
-//   fclose($f);
-// //   mail("robertburek@wp.pl", "Wygrał dzisiaj", $tresc);
-// }
 ?>
